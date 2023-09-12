e862077dacf6c3a32e8959488dcb4737c724f9e5
Sergio Miramon <smiramon8@gmail.com>
2023-09-12 10:15:10 +0200

    refactor playlists

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import "./Navbar.css";
 export const Navbar = () => `
 <nav>
-<a href="#">
+<a href="#" id="homelink">
     <img class="logo" src="/icons/images/all/logo.png" alt="logo">
 </a>
 <ul>
