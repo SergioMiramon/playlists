68520f0121be874ef7541acf55e87b2b7721bef8
Sergio Miramon <smiramon8@gmail.com>
2023-09-12 12:03:11 +0200

    refactor playlists

--- components/PlaylistModal/PlaylistModal.js
+++ components/PlaylistModal/PlaylistModal.js
@@ -4,9 +4,6 @@ export const PlaylistModal = (playlist) => `
 <div class="playlist-modal">
     <div class="modal-name">
         <h2>${playlist.Name}</h2>
-        <a class="arrow" href=".fullplaylists">
-            <i class="arrows icon-arrow-left2"></i>
-        </a>
     </div>
     <div class="modal-info">
     ${playlist.Iframe}
