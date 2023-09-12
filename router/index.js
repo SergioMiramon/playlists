07d1cdd25dc62db8e39fb228c7654d3a66b866c6
Sergio Miramon <smiramon8@gmail.com>
2023-09-12 09:16:29 +0200

    refactor playlists

--- router/index.js
+++ router/index.js
@@ -25,23 +25,23 @@ export const router = () => {
 };
 
 
-window.addEventListener("popstate", router);
+// window.addEventListener("popstate", router);
 
-document.addEventListener("DOMContentLoaded", router);
+// document.addEventListener("DOMContentLoaded", router);
 
-export const addListeners = () => {
-	const navLinks = document.querySelectorAll("nav a");
-	navLinks.forEach((link) => {
+// export const addListeners = () => {
+// 	const navLinks = document.querySelectorAll("nav a");
+// 	navLinks.forEach((link) => {
 
-		link.addEventListener("click", (ev) => {
+// 		link.addEventListener("click", (ev) => {
 	
-			ev.preventDefault();
+// 			ev.preventDefault();
 	
-			const href = link.getAttribute("href");
+// 			const href = link.getAttribute("href");
 	
-			history.pushState(null, null, href);
+// 			history.pushState(null, null, href);
 	
-			router();
-		});
-	});
-};
+// 			router();
+// 		});
+// 	});
+// };
